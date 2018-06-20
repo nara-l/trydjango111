b34b8512066169abc028b9841286ea63d51a95fb
Lawrence Nara <njielitumbe@gmail.com>
2018-06-20 12:03:05 -0400

    Add view for restaurant listing

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from restaurants.views import HomeView
+
+from restaurants.views import restaurant_listview
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^restaurants/', restaurant_listview),
     url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/', TemplateView.as_view(template_name='contact.html'), name='contact')
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -4,13 +4,8 @@ from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-# function based view
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['body'] = "Body content for home"
-        return context
-
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    context = {'object_lists': [1,2,3,455,]}
+    return render(request, template_name, context)
