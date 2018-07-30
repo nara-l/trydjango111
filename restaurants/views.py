689a61bfdb13d277b2234ee5560dd7476437e2b9
Lawrence Nara <njielitumbe@gmail.com>
2018-07-30 07:54:52 -0400

    Add restaurant detail view, slug

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
-
+from django.db.models import Q
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import RestaurantLocation
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 
-def restaurant_listview(request):
-    template_name = 'restaurants/restaurants_list.html'
-    context = {'object_lists': [1,2,3,455,]}
-    return render(request, template_name, context)
+class RestaurantListView(ListView):
+    context_object_name = 'queryset'
+    def get_queryset(self):
+
+        slug = self.kwargs.get('slug')
+        qs = RestaurantLocation.objects.all()
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                                Q(category__iexact=slug)
+                                | Q(category__icontains=slug))
+        else:
+            queryset = qs
+
+        return queryset
+
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id')
+        obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
+        return obj
+
+
+
+
+
